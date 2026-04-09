c331092411cdd875d7b318b088b66aa71a6987c7
Giovana-Manuquian <gmanuquian@gmail.com>
2026-04-09 13:20:37 -0300

    fix: tsconfig and port binding

--- backend/src/services/services.module.ts
+++ backend/src/services/services.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ServicesService } from './services.service';
 import { ServicesController } from './services.controller';
-import { PrismaService } from 'src/prisma.service';
+import { PrismaService } from '../prisma.service';
 
 @Module({
   controllers: [ServicesController],
